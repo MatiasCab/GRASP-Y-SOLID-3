520fce350bdd00cfc592fbb87e08ceae966ce733
Matias <MATIAS.CABRERA@correo.ucu.edu.uy>
2021-09-27 12:53:24 -0300

    Cambios menores en los comentarios

--- src/Library/IPrinter.cs
+++ src/Library/IPrinter.cs
@@ -12,6 +12,6 @@ ya que en la version anterior la unica clase que se encargaba de imprimir las re
 y esto es justamente lo que el patron Polymorfism dice que no hay que hacer. Por lo que cada clase del tipo IPrinter,
 tiene la responsabilidad de imprimir la receta en un destino diferente. Convirtiendo a la operacion "PrintRecipe" en polimorfica.*/
 
-/* Asimsimo, ademas de que el metodo PrintRecipe es polimorfico, el mismo tambien cumplo con el principio solid de LSV,
+/* Asimsimo, ademas de que el metodo PrintRecipe es polimorfico, el mismo tambien cumple con el principio solid LSP,
 ya que el metodo no cambia nungun aspecto de lo propio a imprimir, sino que lo que hace es imprimir la receta
-en ubicaciones diferentes, dependiendo de la clase que se use*/
+en ubicaciones diferentes( dependiendo de la clase que se use).*/
